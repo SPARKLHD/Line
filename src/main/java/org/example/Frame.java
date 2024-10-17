c1e9b511a3edb47f9772b07e21f74eea7b9a92ff
edpanov <egorpanov4002rus@gmail.com>
2024-10-17 09:55:00 +0300

    MVC

--- src/main/java/org/example/Frame.java
+++ src/main/java/org/example/Frame.java
@@ -1,4 +1,16 @@
 package org.example;
 
-public class Frame {
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame extends JFrame {
+    Panel panel;
+
+    public Frame(Panel panel) throws HeadlessException {
+        setResizable(false);
+        add(panel);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1000,1000);
+        setVisible(true);
+    }
 }
--- src/main/java/org/example/Panel.java
+++ src/main/java/org/example/Panel.java
@@ -6,18 +6,23 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionAdapter;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RectangularShape;
 
 public class Panel extends JPanel {
     Point2D[] points;
+    RectangularShape shape;
     public Panel() {
         points = new Point2D[2];
+        shape = new Ellipse2D.Double();
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 super.mousePressed(e);
                 points[0] = e.getPoint();
-                //
+
             }
         });
         addMouseMotionListener(new MouseMotionAdapter() {
@@ -25,7 +30,16 @@ public class Panel extends JPanel {
             public void mouseDragged(MouseEvent e) {
                 super.mouseDragged(e);
                 points[1] = e.getPoint();
+                shape.setFrameFromDiagonal(points[0],points[1]);
+                repaint();
             }
         });
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.draw(shape);
     }
 }
