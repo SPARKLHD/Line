d5f1bdc956de1f03e1ffa2610073a32b8a977d3b
edpanov <egorpanov4002rus@gmail.com>
2024-10-17 09:11:27 +0300

    1

---
+++ src/main/java/org/example/Panel.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Panel extends JPanel {
+    public Panel() {
+        addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+    }
+}
