7293926c876b6261db739a55d506f38bd105102a
edpanov <egorpanov4002rus@gmail.com>
2024-10-24 09:57:38 +0300

    shape without points

--- src/main/java/org/example/model/Shape.java
+++ src/main/java/org/example/model/Shape.java
@@ -8,7 +8,7 @@ import java.awt.geom.RectangularShape;
 //класс фигуры
 public class Shape {
     RectangularShape shape; //объект прямоугольника
-    Point2D p; // начальная точка в 2х мерном пространстве
+    //Point2D p; // начальная точка в 2х мерном пространстве
 
     // конструктор по умолчанию
     public Shape() {
@@ -22,13 +22,13 @@ public class Shape {
 
     //метод для определения первой точки
     public void createShape(Point point) {
-        p = new Point2D.Double(point.x, point.y);
+        shape.setFrameFromDiagonal(new Point2D.Double(point.x, point.y),new Point2D.Double(point.x, point.y));
     }
 
     //метод для определения второй точки
     public void finishShape(Point point) {
-        Point2D finishPoint = new Point2D.Double(point.x, point.y);
-        shape.setFrameFromDiagonal(p, finishPoint); //устанавливаем размер по диагонали
+        Point2D f = (Point2D) point;
+        shape.setFrameFromDiagonal(shape.get); //устанавливаем размер по диагонали
     }
 
     // Добавляем рисовку фигуры
