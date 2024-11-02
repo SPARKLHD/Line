0faf6a45740ecac16b2490242742d143fc97ed5a
egorp <egorpanov4002rus@gmail.com>
2024-11-02 14:57:26 +0300

    1

---
+++ src/main/java/org/example/controller/action/ActionInterface.java
@@ -0,0 +1,5 @@
+package org.example.controller.action;
+
+public interface ActionInterface {
+
+}
--- src/main/java/org/example/view/Frame.java
+++ src/main/java/org/example/view/Frame.java
@@ -9,29 +9,66 @@ import java.awt.event.ActionListener;
 
 public class Frame extends JFrame {
     Panel panel; //для доступа к панели
-    Controller controller;
+    Controller controller; //доступ к контроллеру для кнопок
+
     public Frame(Panel panel, Controller controller) throws HeadlessException {
         this.controller = controller;
-       JMenuBar menuBar = new JMenuBar();
-       setJMenuBar(menuBar);
-       JMenu menu = new JMenu("shape");
-       menuBar.add(menu);
-       JMenuItem itemRec = new JMenuItem("rectangle");
-       JMenuItem itemEll = new JMenuItem("ellipse");
-       menu.add(itemRec);
-       menu.add(itemEll);
-       itemRec.addActionListener(new ActionListener() {
-           @Override
-           public void actionPerformed(ActionEvent e) {
+
+        JMenuBar menuBar = new JMenuBar(); //верхняя полоса для меню
+        setJMenuBar(menuBar); //установка созданного меню
+
+        //создаем кнопку для смены фигуры в меню
+        JMenu menu = new JMenu("shape");
+        menuBar.add(menu); //добавляем в полосу
+
+        //выпадающий список из кнопки
+        JMenuItem itemRec = new JMenuItem("rectangle");
+        JMenuItem itemEll = new JMenuItem("ellipse");
+        menu.add(itemRec);
+        menu.add(itemEll);
+
+        //слушатели для кнопок
+        itemRec.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
                controller.setRectangle();
            }
-       });
-       itemEll.addActionListener(new ActionListener() {
-           @Override
-           public void actionPerformed(ActionEvent e) {
+        });
+        itemEll.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
                controller.setEllipse();
            }
-       });
+        });
+
+
+        //создаем кнопку для смены действия СОЗДАТЬ/ПЕРЕТАЩИТЬ
+        JMenu menu1 = new JMenu("action");
+        menuBar.add(menu1);
+
+        //создаем выпадающие кнопки из action
+        JMenuItem actionDraw = new JMenuItem("draw");
+        JMenuItem actionMove = new JMenuItem("move");
+        menu1.add(actionDraw);
+        menu1.add(actionMove);
+
+        //слушатели для кнопок
+        actionMove.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                controller.setMove();
+            }
+        });
+        actionDraw.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                controller.setDraw();
+            }
+        });
+
+
+
+        //настройки окна
         setVisible(true); //видимость
         setResizable(false); //запрет изменения размера окна
         setSize(1000,1000); //размер окна
